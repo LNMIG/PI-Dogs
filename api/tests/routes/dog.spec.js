4b4e3da34f00cb0c5d9da7430044a7a35e1a5d22
LNMIG <migliorenicolas@yahoo.com.ar>
2022-04-23 00:14:01 -0300

    Testing being finished

--- api/tests/routes/dog.spec.js
+++ api/tests/routes/dog.spec.js
@@ -1,24 +1,67 @@
 /* eslint-disable import/no-extraneous-dependencies */
-const { expect } = require('chai');
+const expect = require('chai').expect;
 const session = require('supertest-session');
 const app = require('../../src/app.js');
-const { Dog, conn } = require('../../src/db.js');
+// const { Dog, conn } = require('../../src/db.js');
+const { Breed, Temper, conn } = require('../../src/db.js');
 
 const agent = session(app);
 const dog = {
-  name: 'Pug',
+  name: 'Camello',
+  height: '162 - 190',
+  weight: '523 - 765',
 };
 
-describe('Videogame routes', () => {
+describe('Dogs PI routes testing', () => {
   before(() => conn.authenticate()
   .catch((err) => {
     console.error('Unable to connect to the database:', err);
   }));
-  beforeEach(() => Dog.sync({ force: true })
-    .then(() => Dog.create(dog)));
-  describe('GET /dogs', () => {
-    it('should get 200', () =>
-      agent.get('/dogs').expect(200)
-    );
+
+  beforeEach(() => Breed.sync({ force: true })
+    .then(() => Breed.create(dog)));
+
+  describe('GET /dogs/:idBreed', () => {
+    it('should respond with 200', () => {
+      return agent.get('/dogs/2').expect(200);
+    });
+    it('should responde with 404', () => {
+      return agent.get('/dogs/200000').expect(404);
+    });
+    it('should responde with string "Sorry, there is no..."', () => {
+      return agent.get('/dogs/200000').expect('Sorry, there is no breed matching your search', true);
+    });
+  });
+  describe('POST /dog', () => {
+    it('should respond with 201 after adding a new breed', () => {
+      return agent.post('/dog')
+      .send({
+        name: 'Chihuahua',
+        height: '20 - 35',
+        weight: '15 - 32',
+        life_span: 'none',
+        image: 'none',
+        temperaments: 'Friendly'
+      }).expect(201);
+    });
+    it('should respond with json file with dog data', () => {
+      return agent.post('/dog')
+      .send({
+        name: 'Chihuahua',
+        height: '20 - 35',
+        weight: '15 - 32',
+        life_span: 'none',
+        image: 'none',
+        temperaments: 'Friendly'
+      })
+      .then(() => {
+        return Breed.findOne({
+          where: { name: 'Chihuahua'}
+        });
+      })
+      .then(dog => {
+        expect(dog).to.exist});
+    });
+    
   });
 });
