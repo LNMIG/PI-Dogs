062295d33f973e5a648457446820a2f4516e9644
LNMIG <migliorenicolas@yahoo.com.ar>
2022-12-07 18:51:00 -0300

    FIX: trying to fix CORS authentication working with app.js file

--- api/src/app.js
+++ api/src/app.js
@@ -19,8 +19,12 @@ server.use((req, res, next) => {
   res.header('Access-Control-Allow-Credentials', 'true');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+  res.header('HTTP/1.1 200 OK');
   next();
 });
+server.options('/*', (_req, res) => {
+  res.sendStatus(200);
+})
 
 server.use('/', routes);
 
