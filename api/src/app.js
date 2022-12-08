bd9e8cfc64c0b305fa408ee46d1b7cfe89599d48
LNMIG <migliorenicolas@yahoo.com.ar>
2022-12-08 13:16:19 -0300

    FIX: cors added

--- api/src/app.js
+++ api/src/app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors');
 const cookieParser = require('cookie-parser');
 const morgan = require('morgan');
 const routes = require('./routes/index.js');
@@ -13,6 +14,7 @@ server.use(express.urlencoded({ extended: true, limit: '50mb' }));
 server.use(express.json({ limit: '50mb' }));
 server.use(cookieParser());
 server.use(morgan('dev'));
+server.use(cors())
 server.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*'); //'http://localhost:3000'); // update to match the domain you will make the request from
   res.header('Access-Control-Allow-Credentials', 'true');
